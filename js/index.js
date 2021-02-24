3ac249991137a03aac4940c20b6d2579bf2431f3
Jonatan Sánchez <jona2255@gmail.com>
2021-02-24 17:18:08 +0100

    Terminada la función equiposPorTipo y borrado un console.log

--- js/index.js
+++ js/index.js
@@ -1,4 +1,15 @@
 function trabajadoresTipo(equipos, tipo) {
   return equipos.filter(equipo => equipo.tipo === tipo);
 }
-console.log(trabajadoresTipo(equipos, "Portátil"));
+function equiposPorTipo(equipos) {
+  const equiposModificados = [];
+  equiposModificados.push({
+    tipo: "Portátil",
+    equipos: equipos.filter(equipo => equipo.tipo === "Portátil")
+  });
+  equiposModificados.push({
+    tipo: "Sobremesa",
+    equipos: equipos.filter(equipo => equipo.tipo === "Sobremesa")
+  });
+  return equiposModificados;
+}
